27d702d9a09d1d74b1d543fbde050a1ce1a97130
NsiriRoua <69841466+NsiriRoua@users.noreply.github.com>
2021-12-30 19:19:08 +0100

    Add files via upload

--- model.py
+++ model.py
@@ -1,166 +1,157 @@
-import pandas as pd 
-import matplotlib.pyplot as plt
-plt.style.use('classic')
+# Import numpy, pandas, matpltlib.pyplot
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px #graphic library
+import datetime
+df = pd.read_csv("NYPD_Complaint_Data_Historic_2019.csv")
+df_backup = df
+columns_remove = ['PREM_TYP_DESC','LOC_OF_OCCUR_DESC','CRM_ATPT_CPTD_CD','LAW_CAT_CD','ADDR_PCT_CD','PD_CD','PD_DESC', 'CMPLNT_NUM', 'OFNS_DESC', 'JURIS_DESC', 'JURISDICTION_CODE', 'PATROL_BORO', 'CMPLNT_TO_DT', 'CMPLNT_TO_TM', 'HADEVELOPT', 'HOUSING_PSA', 'PARKS_NM', 'RPT_DT', 'STATION_NAME', 'TRANSIT_DISTRICT', 'X_COORD_CD','Y_COORD_CD', 'Lat_Lon', 'SUSP_AGE_GROUP', 'SUSP_SEX', 'SUSP_RACE']
+df = df.drop(columns_remove, axis=1)
+df.dropna(subset=['CMPLNT_FR_DT'], inplace=True)
+df.dropna(subset=['CMPLNT_FR_TM'], inplace=True)
+df.replace('UNKNOWN', np.NaN, inplace=True)
+df.replace('E', np.NaN, inplace=True)
+df.replace('D', np.NaN, inplace=True)
+df.replace('U', np.NaN, inplace=True)
+print('Number of rows before removing rows with missing values: ' + str(df.shape[0]))
+df.dropna(axis=0, inplace=True)
+print('Number of rows after removing rows with missing values: ' + str(df.shape[0]))
+df['CMPLNT_FR_Year'] = df['CMPLNT_FR_DT'].map(lambda x: int(str(x).split('-')[0]))
+df['CMPLNT_FR_Month'] = df['CMPLNT_FR_DT'].map(lambda x: int(x.split('-')[1]))
+df['CMPLNT_FR_Day'] = df['CMPLNT_FR_DT'].map(lambda x: int(x.split('-')[2]))
+df['CMPLNT_FR_Hour'] =df['CMPLNT_FR_TM'].map(lambda x: int(x.split(':')[0]))
+df['CMPLNT_FR_DT'] = pd.to_datetime(df['CMPLNT_FR_DT'], errors='coerce')
+df['Weekday']=df['CMPLNT_FR_DT'].dt.strftime('%a')
+df['Weekday'].unique()
+from sklearn.preprocessing import LabelEncoder
+df_sel1 = pd.DataFrame(df, columns=['Weekday'])
+labelencoder = LabelEncoder()
+df_sel1['Weekday_cat'] = labelencoder.fit_transform(df['Weekday'])
+df['Weekday']=df_sel1.iloc[:,1:]
+columns_remove = ['CMPLNT_FR_TM', 'CMPLNT_FR_DT']
+df = df.drop(columns_remove, axis=1)
+def ky_cat(ky_cd):
+    if ky_cd in [101,102,103]:
+        return "HOMICIDE"
+    elif ky_cd in [104,115,116,233,234,356,460]:
+        return "SEXCRIME"
+    elif ky_cd in [105,107,109,110,111,112,113,231,238,340,341,342,343,358]:
+        return "THEFTFRAUD"
+    elif ky_cd in [106,114,124,344]:
+        return "OTHERVIOLENT"
+    elif ky_cd in [117,118,119,232,235,236,346,347,577]:
+        return "DRUGS"
+    elif ky_cd in [120, 121, 125, 126, 345, 345, 348, 349, 351, 352, 353, 354, 355, 357, 359, 360, 361, 362, 363, 364, 364, 364, 365, 366, 455, 571, 572, 578, 672, 675, 676, 677, 677, 678, 685, 881] :
+        return "OTHER"
+df['KY_CD'] = df['KY_CD'].map(lambda x: ky_cat(x))
+dum_df = pd.get_dummies(df, columns=["KY_CD","BORO_NM","VIC_SEX","VIC_RACE","VIC_AGE_GROUP"], prefix=["KY_CD","BORO_NM","VIC_SEX","VIC_RACE","VIC_AGE_GROUP"] )
+# Get the feature vector
+X = dum_df.drop(['KY_CD_DRUGS', 'KY_CD_HOMICIDE', 'KY_CD_OTHER', 'KY_CD_OTHERVIOLENT', 'KY_CD_SEXCRIME', 'KY_CD_THEFTFRAUD'], axis = 1)
 
-df = pd.read_csv("NYPD_Complaint_Data_Historic.csv")
+# Get the target vector
+y = dum_df[['KY_CD_DRUGS', 'KY_CD_HOMICIDE', 'KY_CD_OTHER', 'KY_CD_OTHERVIOLENT', 'KY_CD_SEXCRIME', 'KY_CD_THEFTFRAUD']]
 
-
-df.fillna(df.median(), inplace=True)
-df = df.dropna()
-df.reset_index()
-df = pd.get_dummies(df,columns=['PATROL_BORO'])
-df = pd.get_dummies(df,columns=['VIC_SEX'])
-df = pd.get_dummies(df,columns=['VIC_RACE'])
-import datetime 
-import calendar
-result_month=[]
-result_year=[]
-result_day=[]
-for i in df['CMPLNT_FR_DT']:
-    month,day,year=i.split('/')
-    result_month.append(month)
-    result_year.append(year)
-    result_day.append(day)
-df['month']=result_month
-df['year']=result_year
-df['day']=result_day
-VIC_AGE_GROUP_mapping ={'25-44':0, '45-64':1, '<18':2, '18-24':3, 'UNKNOWN':4, '65+':5,'1014':6, '-931':7, '-972':8}
-df['VIC_AGE_GROUP']=df['VIC_AGE_GROUP'].map(VIC_AGE_GROUP_mapping)
-df = df[df.VIC_AGE_GROUP != 8]
-df = df[df.VIC_AGE_GROUP != 7]
-df = df[df.VIC_AGE_GROUP != 6]
-all_crimes = ["assualt","grand larency","petit larency",
-        "harrasment","exposed to weapons","criminal crimes","public safty crimes","administrative crimes","vehical crimes",
-        "drugs and alcaholic crimes","theif and robbery ","kidnapping","frauds","children crimes"]
-df_mapping ={#assualt
-           'ASSAULT 3 & RELATED OFFENSES':0,'FELONY ASSAULT':0,'OFFENSES AGAINST THE PERSON':0,
-             #grand larency
-             'GRAND LARCENY':1, 
-             #petit larency
-             'PETIT LARCENY':2,'PETIT LARCENY OF MOTOR VEHICLE':2,
-             #harrasment
-            'HARRASSMENT 2':3,
-            #sex crime
-           'SEX CRIMES':4,'RAPE':4,'PROSTITUTION & RELATED OFFENSES':4,
-             #exposed to weapons
-            'DANGEROUS WEAPONS':5, 'BURGLARY':5,
-             #criminal crimes
-       'CRIMINAL MISCHIEF & RELATED OF':7,'CRIMINAL TRESPASS':7,
-        #public safty crimes
-       'OFF. AGNST PUB ORD SENSBLTY &':9, 'OFFENSES AGAINST PUBLIC SAFETY':9, 'LEWDNESS,PUBLIC':9, 'ARSON':9,'JOSTLING':9, 
-        #administrative crimes
-       'MISCELLANEOUS PENAL LAW':11, 'OFFENSES AGAINST PUBLIC ADMINI':11,  'ADMINISTRATIVE CODE':11,
-        #vehical crimes     
-       'VEHICLE AND TRAFFIC LAWS':13, 'DISORDERLY CONDUCT':13,'HOMICIDE-NEGLIGENT-VEHICLE':13,'UNAUTHORIZED USE OF A VEHICLE':13,'GRAND LARCENY OF MOTOR VEHICLE':13, 'INTOXICATED & IMPAIRED DRIVING':13, 
-        #drugs and alcaholic crimes 
-        'DANGEROUS DRUGS':16,'ALCOHOLIC BEVERAGE CONTROL LAW':16,
-        #theif and robbery
-       'OTHER OFFENSES RELATED TO THEF':17,'THEFT-FRAUD':17,'THEFT OF SERVICES':17, 'ROBBERY':17,'POSSESSION OF STOLEN PROPERTY':17,"BURGLAR'S TOOLS":17,
-       #kidnapping
-       'KIDNAPPING & RELATED OFFENSES':27,
-        #frauds
-       'FORGERY':30,'FRAUDULENT ACCOSTING':30, 'FRAUDS':30,'OFFENSES INVOLVING FRAUD':30,
-         #childern crimes
-        'OFFENSES RELATED TO CHILDREN':37,'CHILD ABANDONMENT/NON SUPPORT':37,
-       'AGRICULTURE & MRKTS LAW-UNCLASSIFIED':41,'OTHER STATE LAWS (NON PENAL LA':36,'NYS LAWS-UNCLASSIFIED FELONY':29,'FALSE REPORT UNCLASSIFIED':47,'HOMICIDE-NEGLIGENT,UNCLASSIFIE':52, 'THEFT OF SERVICES, UNCLASSIFIE':54,'LOITERING':43, 'OTHER STATE LAWS':55,'NOISE,UNECESSARY':49, 'TAX LAW':50, 'NYS LAWS-UNCLASSIFIED VIOLATION':51,'ENDAN WELFARE INCOMP':45,'GAMBLING':35}
-
-df['OFNS_DESC']=df['OFNS_DESC'].map(df_mapping)
-df = df[df.OFNS_DESC != 43]
-df = df[df.OFNS_DESC != 48]
-df = df[df.OFNS_DESC != 49]
-df = df[df.OFNS_DESC != 50]
-df = df[df.OFNS_DESC != 55]
-df = df[df.OFNS_DESC != 52]
-df = df[df.OFNS_DESC != 54]
-df = df[df.OFNS_DESC != 41]
-df = df[df.OFNS_DESC != 51]
-
-df = df[df.OFNS_DESC != 36]
-df = df[df.OFNS_DESC != 45]
-df = df[df.OFNS_DESC != 47]
-df = df[df.OFNS_DESC != 29]
-df = df[df.OFNS_DESC != 35]
-L=["morning","afternoon","evening","night"]
-result=[]
-for x in df.CMPLNT_FR_TM:
-        
-    times=x.split(':')
-    if int(times[0])>6 and int(times[0])<=12:
-        result.append(L[0])
-    elif int(times[0])>12 and int(times[0])<=17:
-        result.append(L[1])
-    elif int(times[0])<20:
-        result.append(L[2])
-    else : 
-        result.append(L[3])
-df['time_zone']=result
-
-time_zone_mapping ={'afternoon':0, 'morning':1, 'evening':2, 'night':3}
-df['time_zone']=df['time_zone'].map(time_zone_mapping)
-
-df.dropna()
-df = df.fillna(df.median())
-
+print('X shape: ' + str(X.shape))
+print('y shape: ' + str(y.shape))
 from sklearn.model_selection import train_test_split
 
-#Split dataset to Training Set & Test Set
-x, y = train_test_split(df, 
-                        test_size = 0.2, 
-                        train_size = 0.8, 
-                        random_state= 3)
-Features=['Latitude', 'Longitude',
-       'VIC_AGE_GROUP', 'VIC_SEX_D', 'VIC_SEX_E',
-       'VIC_SEX_F', 'VIC_SEX_M', 'VIC_RACE_AMERICAN INDIAN/ALASKAN NATIVE',
-       'VIC_RACE_ASIAN / PACIFIC ISLANDER', 'VIC_RACE_BLACK',
-       'VIC_RACE_BLACK HISPANIC', 'VIC_RACE_UNKNOWN', 'VIC_RACE_WHITE',
-       'VIC_RACE_WHITE HISPANIC', 'month', 'year', 'day', 'time_zone']
-Target= ['OFNS_DESC']
+# Randomly choose 30% of the data for testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0, stratify=y)
 
-x1 = x[Features]    #Features to train
-x2 = x[Target]      #Target Class to train
-y1 = y[Features]    #Features to test
-y2 = y[Target]      #Target Class to test
+# Show the shape of the data
+print('y train shape: ' + str(np.unique(y_train, return_counts=True)))
+print('y test shape: ' + str(np.unique(y_test, return_counts=True)))
+from sklearn.model_selection import train_test_split
 
-print('Feature Set Used    : ', Features)
-print('Target Class        : ', Target)
-print('Training Set Size   : ', x.shape)
-print('Test Set Size       : ', y.shape)
+# Randomly choose 30% of the data for testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0, stratify=y)
+
+# Show the shape of the data
+print('y train shape: ' + str(np.unique(y_train, return_counts=True)))
+print('y test shape: ' + str(np.unique(y_test, return_counts=True)))
+from sklearn.utils.multiclass import type_of_target
+print(type_of_target(y_train.idxmax(axis=1)))
+print(type_of_target(y_train))
+y_train.idxmax(axis=1)
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau,ModelCheckpoint
+MCP = ModelCheckpoint('Best_points.h5',verbose=1,save_best_only=True,monitor='val_accuracy',mode='max')
+ES = EarlyStopping(monitor='val_accuracy',min_delta=0,verbose=1,restore_best_weights = True,patience=3,mode='max')
+RLP = ReduceLROnPlateau(monitor='val_loss',patience=3,factor=0.2,min_lr=0.0001)
+from keras.layers.core import Dense, Dropout
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers import Dense, BatchNormalization
+import keras
+LR = 0.0001
+# Create model
+model = Sequential()
+model.add(Dense(512, input_dim=55, activation='relu'))
+model.add(Dense(256,activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(6, activation='softmax'))
+
+# Compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Fit model to training data
+model.fit(X_train, y_train, epochs=10, batch_size=512)
+
+# Evaluate model on test data
+scores = model.evaluate(X_test, y_test)
+print("\n%s: %.14f%%" % (model.metrics_names[1], scores[1]*100))
+
+# Import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_selection import SelectFromModel
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_curve, auc
+import math
+#Create a Gaussian Classifier
+clf=RandomForestClassifier(n_estimators=100, # Number of trees
+                            min_samples_split = 20,
+                            bootstrap = True, 
+                            max_depth = 50, 
+                            min_samples_leaf = 25)
+
+#Train the model using the training sets y_pred=clf.predict(X_test)
+clf.fit(X_train,y_train)
+
+y_pred=clf.predict(X_test)
+
+# Get the accuracy score
+acc_rf=accuracy_score(y_test, y_pred)
+
+# Model Accuracy, how often is the classifier correct?
+print("[Random forest algorithm] accuracy_score: {:.3f}.".format(acc_rf))
+cols_results=['family','model','classification_rate','runtime']
+results = pd.DataFrame(columns=cols_results)
 
 
-# Random Forest
-# Create Model with configuration
-from sklearn.ensemble import RandomForestClassifier,VotingClassifier
-
-rf_model = RandomForestClassifier(n_estimators=30, # Number of trees
-                                  min_samples_split = 20,
-                                  bootstrap = True, 
-                                  max_depth = 50, 
-                                  min_samples_leaf = 25)
-
-# Model Training
-rf_model.fit(X=x1,y=x2)
-
-# Prediction
-result = rf_model.predict(y[Features])
-
-
-from sklearn.metrics import precision_score, recall_score, confusion_matrix, classification_report, accuracy_score, f1_score
-# Model Evaluation
-ac_sc = accuracy_score(y2, result)
-rc_sc = recall_score(y2, result, average="weighted")
-pr_sc = precision_score(y2, result, average="weighted")
-f1_sc = f1_score(y2, result, average='micro')
-confusion_m = confusion_matrix(y2, result)
-
-print("========== Random Forest Results ==========")
-print("Accuracy    : ", ac_sc)
-print("Recall      : ", rc_sc)
-print("Precision   : ", pr_sc)
-print("F1 Score    : ", f1_sc)
-print("Confusion Matrix: ")
-print(confusion_m)
+from sklearn.neighbors import KNeighborsClassifier
+import time
+kVals = range(1,10)
+knn_names = ['KNN-'+str(k) for k in kVals]
+for k in kVals:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    
+    
+    time_start = time.time()
+    y_pred = knn.predict(X_test)
+    time_run = time.time()-time_start
+    
+    results = results.append(pd.DataFrame([['KNN',knn_names[k-1],accuracy_score(y_test,y_pred),time_run]],columns=cols_results),ignore_index=True)
+results[results.family=='KNN']
+from sklearn.dummy import DummyClassifier
+clf = DummyClassifier(strategy='stratified',random_state=0)
+clf.fit(X_train,y_train)
+y_pred = clf.predict(X_test)
+print('Accuracy of a random classifier is: %.2f%%'%(accuracy_score(y_test,y_pred)*100))
 
 from flask import Flask , render_template , request , flash , redirect , url_for
 from folium.plugins import Draw
@@ -245,7 +236,7 @@ def getTime(time):
         return 3
 def getKeyFromValue(arg):
     t = []
-    for k , v in df_mapping.items():
+    for k , v in df_backup.items():
         if v == arg :
             t.append(k)
     return t 
@@ -271,9 +262,9 @@ def index():
        VIC_SEX_M, a , b , c , d , e , f , g , trip_time.month, trip_time.year, trip_time.day, time]
         coords = [Latitude , Longitude ]
         input = [input]
-        result1 = rf_model.predict(input)
-        proba = rf_model.predict_proba(input)
-        CrimesList = dict(zip(all_crimes, proba[0]))
+        result1 = clf.predict(input)
+        proba = clf.predict_proba(input)
+        CrimesList = dict(zip(y, proba[0]))
         return render_template("alert.html", crimes = CrimesList)
         
     return render_template("main_page.html", coords = coords )
